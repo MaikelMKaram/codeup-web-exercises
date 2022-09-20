1985bf39763f7ad3d4ec9b316fb4572326981d3e
Maikel Karam <maikel.m.karam@gmail.com>
2022-09-20 14:46:19 -0500

    saving progress for circle exercises

--- js/circle.js
+++ js/circle.js
@@ -33,6 +33,29 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
 
+    var circle = {
+        radius: 5,
+
+        getArea: function () {
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return Math.PI * Math.pow(this.radius,2); // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+            if(doRounding === true){
+                // If doRounding is true, round the result to the nearest integer.
+                // Otherwise, output the complete value
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(this.getArea()));
+            }  else {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea())
+            }
+        }
+    };
+
+
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
