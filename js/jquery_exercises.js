89683db9b655e8863ae8fc97f6688736c08612e8
Maikel Karam <maikel.m.karam@gmail.com>
2022-10-19 14:51:15 -0500

    JQuery Class Selection exercise completed

---
+++ js/jquery_exercises.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// $(function() {
+//     alert( 'The DOM has finished loading!' );
+// });
+
+//Id Selectors
+// let contents = $('#list').html();
+// alert(contents);
+
+//Class Selectors
+// $('.codeup').css('border','red 1px solid');
+
+//Elements Selectors
+// $('li').css('font-size', '20px');
+// $('h1, p, li').css('border', 'blue 1px solid');
+// alert($('h1').html());
