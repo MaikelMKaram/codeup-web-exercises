16d23f203aade781513488e86e6746c89fb0c2eb
Maikel Karam <112984309+MaikelMKaram@users.noreply.github.com>
2022-10-25 14:22:19 -0500

    Delete api-key.js

--- js/api-key.js
+++
@@ -1,5 +0,0 @@
-//mapbox api
-const key = "pk.eyJ1IjoibWFpLWthIiwiYSI6ImNsOW41N2E1ZDAxZDYzdm84cnpwd3c4ZXEifQ.4T_4IwYC9IRcS6U3zTZI_A";
-
-//weatherMap api
-const weatherKey = "5e04bce5e5f09c6f1ed39b847cf298a1";
