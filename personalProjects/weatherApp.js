8d5b882b8cd3c26eddc922fbf35f584bca1105a6
Maikel Karam <maikel.m.karam@gmail.com>
2022-10-26 16:48:57 -0500

    weatherApp progress save

---
+++ personalProjects/weatherApp.js
@@ -0,0 +1,28 @@
+const timeElement = document.getElementById('time');
+const dateElement = document.getElementById('date');
+const currentWeatherItemsElement = document.getElementById('current-weather-items');
+const timezone = document.getElementById('time-zone');
+const countryElement = document.getElementById('country');
+const weatherForecastElement = document.getElementById('weather-forecast');
+const currentTempElement = document.getElementById('current-temp');
+
+const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+//Interval function Updates every 1 min
+setInterval(() => {
+    // Declarations
+    const time = new Date();
+    const month = time.getMonth();
+    const date = time.getDate();
+    const day = time.getDay();
+    const hour = time.getHours();
+    const hoursIn12HrFormat = hour >= 13 ? hour % 12: hour
+    const minutes = time.getMinutes();
+    const ampm = hour >=12 ? 'PM' : 'AM'
+
+    //Time Element
+    timeElement.innerHTML = hoursIn12HrFormat + ":" + minutes +  " " + ampm
+    //Date Element
+    dateElement.innerHTML = `${days[day]}, ${date} ${months[month]}`
+}, 1000)
