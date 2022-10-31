578809e50dcb46243fd81606bcb5b8454d50bfd4
Maikel Karam <maikel.m.karam@gmail.com>
2022-10-31 15:12:55 -0500

    Weather App progress

--- personalProjects/weatherApp.js
+++ personalProjects/weatherApp.js
@@ -88,7 +88,7 @@ function mainFunction(e){
     marker.setLngLat(coordinates).addTo(map);
     map.flyTo({center:coordinates,
         zoom: 8,
-        duration: 3000,
+        duration: 2000,
     });
     // var input = coordinates
     $.ajax({
