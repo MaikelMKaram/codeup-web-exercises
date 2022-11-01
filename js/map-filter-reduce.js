2aff3db3ee38d22879a45d598f73ee5c850c9870
Maikel Karam <maikel.m.karam@gmail.com>
2022-11-01 10:15:51 -0500

    map-filter-reduce progress save

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -72,6 +72,21 @@ console.log(longestEmail);
 //Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
 
 let names = users.reduce(
-    (stack, user) => {return stack += ` ${user.name}`; }, ''
+    (list, user) => {return list += ` ${user.name}`;}, ''
 );
 console.log(names);
+
+//Use .reduce to get the unique list of languages from the list of users.
+
+let uniqueLang = users.reduce((languages, usersLang) => {
+        usersLang.languages.forEach(function (language) {
+            if (languages.includes(language)){
+            } else {
+                languages.push(language);
+            }
+        });
+        return languages
+    }, []);
+console.log(uniqueLang);
+
+
